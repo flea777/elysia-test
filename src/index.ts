900f282188f469a352f871c189b04c31efc8b786
Pedro Bim <pedrodinizz2@outlook.com>
2025-07-28 14:32:25 -0300

    Correção

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import { Elysia } from 'elysia'
 
-const app = new Elysia().get('/test', ({ body }) => {
-  console.log(JSON.stringify(body))
-}).listen(3000)
+const app = new Elysia()
+  .post('/test', ({ body }) => {
+    console.log('Corpo recebido:', body)
+    return { message: 'Dados recebidos com sucesso!', data: body }
+  })
+  .get('/hello', () => {
+    return { message: 'Hello World!' }
+  })
+  .listen(3000)
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
